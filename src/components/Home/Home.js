8146b4b9de2a2918f9f4b88bf9e4bada444fe189
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2021-12-28 19:38:36 +0600

    first commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Card, Figure, Image } from 'react-bootstrap';
+import farhan from '../../Images/farhan.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
+    
+    <div className='container'>
         <div>
-            <p>This Is Home</p>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of
+        the card's content.
+        </Card.Text>
         </div>
+
+        <div>
+        <Figure>
+  <Figure.Image
+    width={471}
+    height={280}
+    alt="171x180"
+    src={farhan}
+  />
+ 
+</Figure>
+                
+
+        </div>
+
+    </div>
+     
+        
+    
     );
 };
 
